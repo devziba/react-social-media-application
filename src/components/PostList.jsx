69056838bee0d51b3b16e58f9842d9d302e542de
devziba <devwork64@gmail.com>
2025-07-02 23:08:23 +0530

    more polished

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -3,20 +3,19 @@ import { PostList as postListData } from "../store/post-list-store";
 import Post from "./Post";
 import WelcomeMessage from "./WelcomeMessage";
 function PostList() {
-  let { postList , addInitialPosts } = useContext(postListData);
+  let { postList, addInitialPosts } = useContext(postListData);
   let posts = postList;
 
   if (posts.length === 0) {
     const handleGetPostClick = () => {
       fetch("https://dummyjson.com/posts")
         .then((res) => res.json())
-        .then(data =>{
-
-          data.posts.forEach((value)=>{
-            addInitialPosts(value)}
-          )
-          }
-          ).catch((error) => console.error("Failed to fetch posts:", error));
+        .then((data) => {
+          data.posts.forEach((value) => {
+            addInitialPosts(value);
+          });
+        })
+        .catch((error) => console.error("Failed to fetch posts:", error));
     };
     return (
       <WelcomeMessage onGetPostsClick={handleGetPostClick}></WelcomeMessage>
